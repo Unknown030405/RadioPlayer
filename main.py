c37d9f509c6120ebecd85cb06ae6207efc7676c8
unknown030405 <dpsdanilPar@gmail.com>
2022-04-05 17:30:07 +0500

    first commit (registration form)

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import *
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def succes():
+    return render_template('personal.html', title='Личный Кабинет')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host="127.0.0.1")
